11751c59709579d2a3d5ad18cf31dd9dc96ab182
Clayman <Clayman@DESKTOP-DM8E77U>
2022-02-20 16:46:43 +0200

    DataTypes start.

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << true << endl;
+	cout << false << endl;
+}
+
+
+
+//char (Character - Символ) 1 Byte ASCII 256
